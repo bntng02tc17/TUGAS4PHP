6e9d66c37314c56cf334cc3d2a8dcc0297fcdae2
bntng02tc17 <37019764+bntng02tc17@users.noreply.github.com>
2018-09-30 23:35:40 +0700

    Update date.php

--- date.php
+++ date.php
@@ -9,6 +9,9 @@
         echo "selamat Pagi<br>";
     }
     elseif(date("H")<15){
+        echo "Selamat Siang<br>";
+    }
+     elseif(date("H")<18){
         echo "Selamat Sore<br>";
     }
     else {
